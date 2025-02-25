09a2225fa6263de284992ed25bf6e236c84aebf0
kotetsu103 <jmfranco103@gmail.com>
2025-02-25 17:32:05 -0500

    Jose Franco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NosotrosComponent } from "./nosotros/nosotros.component";
+import { ContactosComponent } from './contactos/contactos.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NosotrosComponent],
+  imports: [RouterOutlet, NosotrosComponent, ContactosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
